6f6e3f15efb9766a7ef3df9ee43e8ef033ca639b
verdulife <verdu@live.com>
2021-09-27 23:40:26 +0200

    ocr testing page

---
+++ src/routes/ocr/index.svelte
@@ -0,0 +1,16 @@
+<script>
+  import { createWorker } from "tesseract.js";
+
+  const worker = createWorker({
+    logger: (m) => console.log(m),
+  });
+
+  (async () => {
+    await worker.load();
+    await worker.loadLanguage("eng");
+    await worker.initialize("eng");
+    const all = await worker.recognize("/invoice.png");
+    console.log(all);
+    await worker.terminate();
+  })();
+</script>
